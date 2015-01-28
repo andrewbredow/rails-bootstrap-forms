8e843d7cc4831b01981bb962e091e4b8f2835338
Andrew Bredow <bredow@gmail.com>
2015-01-28 15:30:39 -0500

    Allow the label option to be passed in as a hash
    When generating fields, it should be possible to specify a hash in the
    format of `label: { text: "A label", class: "a-class" }`, as indicated
    in the documentation.
    
    This approach assumes that if the developer has specified the label as a
    hash, then it trumps `label_class` in the options hash.

--- lib/bootstrap_form/form_builder.rb
+++ lib/bootstrap_form/form_builder.rb
@@ -304,11 +304,19 @@ module BootstrapForm
       end
 
       unless options.delete(:skip_label)
-        label_class = hide_class if options.delete(:hide_label)
+        if options[:label].is_a?(Hash)
+          label_text  = options[:label].delete(:text)
+          label_class = options[:label].delete(:class)
+        end
         label_class ||= options.delete(:label_class)
+        label_class = hide_class if options.delete(:hide_label)
 
-        form_group_options.reverse_merge!(label: {
-          text: options.delete(:label),
+        if options[:label].is_a?(String)
+          label_text ||= options[:label]
+        end
+
+        form_group_options.merge!(label: {
+          text: label_text,
           class: label_class
         })
       end
